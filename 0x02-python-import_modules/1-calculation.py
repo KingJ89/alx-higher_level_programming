ea372c2c9b319372d27178c7b32bf1009a4adde0
KingJ89 <janmutewera@gmail.com>
2023-12-01 12:22:54 -0800

    task 1 completed sucessfully

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    """print the sum, difference, multiple and quotientof 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".fomat(a, b, add(a, b)))
+    print("{} + {} = {}".fomat(a, b, sub(a, b)))
+    print("{} + {} = {}".fomat(a, b, mul(a, b))) 
+    print("{} + {} = {}".fomat(a, b, dov(a, b)))
