fc069deeffb58a8009ea7fbc78407d453168c532
KingJ89 <janmutewera@gmail.com>
2023-12-07 13:39:08 -0800

    all tasks completed,ccompiled and ammended successfully

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,25 @@
 #include "lists.h"
 
 /**
- * check_cycle -checks if singly linked list has cycle
- * @list: pointer to head of linked list
- *
- * Return 1 if there is a cycle, 0 otherwise
+ * check_cycle - checks cycle and cyclic redundancies
+ * @list: linked list head
+ * Return: 0on error, 1 on success
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise =list;
-	listint_t *hare = list;
+	listint_t *top, *bottom;
 
-	while (hare != NULL && hare->next != NULL)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
+	if (!list || !list->next)
+		return (0);
 
-		if (tortoise ==hare)
+	top = list;
+	bottom = list->next;
+	while (bottom && bottom->next)
+
+		if (top == bottom)
 			return (1);
-	}
+
+	top = top->next;
+	bottom = bottom->next->next;
 	return (0);
 }
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print ((str + '\n') * 3, end='')
-
-print (str[:9] + '\n')
-
+print(str *3 +"\n" + str[:9])
