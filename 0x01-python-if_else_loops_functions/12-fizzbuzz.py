3e1b70854d8e13c247db99c2d1b15c4b849b9965
KingJ89 <janmutewera@gmail.com>
2023-12-10 08:08:38 -0800

    task 12 ammended successfully

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """prints the fizz buzz  solution for the fizz buzz interview question"""def fizzbuzz():
     for j in range(1, 101):
-        if j $ 3 == 0 and j % 5 == 0:
+        if j % 3 == 0 and j % 5 == 0:
             print("FizzBuzz ", end="")
 
         elif:
