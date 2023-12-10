734d58d43e3c248a1a07db4783d209876f92e2a2
KingJ89 <janmutewera@gmail.com>
2023-12-10 11:45:22 -0800

    task 2 args sucessfully debugged

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    #print number of list of args
+    # Print the number of arguments in the list
     import sys
 
     count = len(sys.argv) - 1
     if count == 0:
         print("0 args.")
     elif count == 1:
-        print("1 args:")
+        print("1 arg:")
     else:
-        print("{} args:" .format(count))
+        print("{} args:".format(count))
         for i in range(count):
-            print("{}: {}" .format(i + 1, sys.argv[i + 1]))
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
