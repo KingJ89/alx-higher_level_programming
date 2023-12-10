ff64448e55cecbeb821cdfdd257d0928190f8515
KingJ89 <janmutewera@gmail.com>
2023-12-10 11:38:22 -0800

    task 1 calc amended successfuly and debugged

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    #print the sum, difference, multiple and quotientof 10 and 5.
+    # Print the sum, difference, product, and quotient of 10 and 5.
     from calculator_1 import add, div, mul, sub
 
     a = 10
@@ -9,5 +8,6 @@ if __name__ == "__main__":
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b))) 
+    print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
