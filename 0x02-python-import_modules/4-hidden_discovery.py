4b30ae47e52b170f98da344cab406688e38b292a
KingJ89 <janmutewera@gmail.com>
2023-12-10 11:10:24 -0800

    task 4 hidden amended successfully 2nd

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-        """prints names in the compiled module"""
-        import hidden_4
+"""Prints names in the compiled module"""
 
-        if __name__ == "__main__":
+import hidden_4
 
-                file = dir(hidden_4)
-                for name in file:
-                if name[:2] !+ "--":
-                print(file)
+if __name__ == "__main__":
+    file = dir(hidden_4)
+    for name in file:
+        if name[:2] != "__":
+            print(name)
