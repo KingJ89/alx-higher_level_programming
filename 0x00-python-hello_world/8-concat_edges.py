c52b59bd5a2ebcf9c6bb53326bc2ff21589fbd8c
KingJ89 <janmutewera@gmail.com>
2023-12-31 01:09:45 -0800

    task 8 chores and debugging completed

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[70:119] + str[0:6] + str[-22:-17] + str[50:52])
+print(str[39:67] + str[107:112] + str[0:6] + str[6:6])
