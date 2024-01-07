0233e9ea349c71d7515f9ec89c226f976f2cd1b4
KingJ89 <janmutewera@gmail.com>
2024-01-07 01:51:03 -0800

    task 2 chores completed successfully

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Print the number of arguments in the list
+    """Prints the result of the addition of arguments"""
     import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 args.")
-    elif count == 1:
-        print("1 arg:")
-    else:
-        print("{} args:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    arguments = sys.argv[1:]
+    count = len(arguments)
 
+    # Print the number of arguments
+    print(f"{count} {'argument' if count == 1 else 'arguments'}:")
+
+    # Print each argument with its index
+    for i, arg in enumerate(arguments, start=1):
+        print(f"{i}: {arg}")
