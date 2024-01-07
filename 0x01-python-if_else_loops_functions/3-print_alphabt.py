da8949f59b6c39fdf0e67820c146411178cfcbf0
KingJ89 <janmutewera@gmail.com>
2024-01-07 00:33:08 -0800

    task 3 amended  successfully

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for j in range(97, 123):
-    if chr(j) != 'q' and chr(j) != 'e':
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
         print("{}".format(chrj)), end="")
