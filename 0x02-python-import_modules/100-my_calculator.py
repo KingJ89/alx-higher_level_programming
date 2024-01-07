81ead4745dbcf604df960c82b5bfe9b1429c8644
KingJ89 <janmutewera@gmail.com>
2024-01-07 01:56:58 -0800

    task 100 chores completed successfully

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Handles basic operations."""
+    """Handles basic operations"""
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in ops:
+    operator = sys.argv[2]
+
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a, operator, b = map(int, sys.argv[1:])
-    result = ops[operator](a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
+    try:
+        a, b = map(int, sys.argv[1:4])
+    except ValueError:
+        print("Invalid input. Please provide valid integers for <a> and <b>")
+        sys.exit(1)
 
+    if operator == "/" and b == 0:
+        print("Error: Division by zero is not allowed")
+        sys.exit(1)
+
+    result = ops[operator](a, b)
+    print(f"{a} {operator} {b} = {result}")
