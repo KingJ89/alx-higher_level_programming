4f1a8b11f36fa0f5390e57ad8f0fe82c08733d45
KingJ89 <janmutewera@gmail.com>
2024-01-07 01:00:26 -0800

    task 6 chores completed successfully

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for j in range(0, 10):
     for m in range(j + 1, 10):
-        if j == 8 and m == 9:
-            print("{}{}".format(j, m))
-        else:
-            print("{}{}".format(j, m), end=",")
+    if j == 8 and m == 9:
+    print("{}{}".format(j, m))
+    else:
+    print("{}{}".format(j, m), end=",")
