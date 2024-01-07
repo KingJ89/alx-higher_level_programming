952c43f4c80711795c04f243dee80ca1cd712cf6
KingJ89 <janmutewera@gmail.com>
2024-01-07 00:57:47 -0800

    task 6 chores completed successfully

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#print pairs of values j, m where j is less than m and seperated by commas
 for j in range(0, 10):
     for m in range(j + 1, 10):
         if j == 8 and m == 9:
-            print("{}{}".format{j, m))
+            print("{}{}".format(j, m))
         else:
             print("{}{}".format(j, m), end=",")
