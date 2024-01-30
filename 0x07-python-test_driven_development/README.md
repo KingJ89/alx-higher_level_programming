ff122a779097fafc836b53d4d5b04b54345f0b8c
KingJ89 <janmutewera@gmail.com>
2024-01-29 17:54:54 -0800

    task 0 completed successfully

---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+this is a non empty readme file on the Alx 0x07 python test driven development project by Jan Mutewera
--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,2 +1,6 @@
+#!/usr/bin/python3
+"""module defining an empty class Rectangle."""
+
 class Rectangle:
+    """empty class rectangle."""
     pass
