1c4e8d258f0e0776e58e3be344e5bdaf07f94cf5
KingJ89 <janmutewera@gmail.com>
2024-02-01 16:35:02 -0800

    Task 1 chores handled

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-from main import Rectangle
+
+"""
+create class Rectangle that defines a rectangle
+"""
+
 class Rectangle:
+    """Rectangle class"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
+        @property
         def width(self):
             return self.__width
 
+        @width.setter
         def width(self, value):
-            if not isinsistance(value, int):
-                raise TypeError("width must be integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-                self.__width = value
+            self._validate_non_negative_integer("width", value)
+            self.__width = value
 
-                def height(self):
-                    return self.height
+            @property
+            def height(self):
+                return self.__height
 
-                def height(self, value):
-                    if not isinsance(value, int):
-                        raise TypeError("height must be an integer")
-                    elif Value < 0:
-                        else:
-                            self.__height = value
+            @height.setter
+            def height(self, value):
+                self._validate_non_negative_integer("height", value)
+                self.__height = value
+
+                def _validate_non_negative_integer(self, attibute, value):
+                    if not isinstance(value, int):
+                        raise TypeError(f"{attribute} must be an integer")
+                    if value < 0:
+                        raise ValueError(f"{attribute} must be >= 0")
+                    if __name__ == "__main__":
+                        rect1 = Rectangle(2, 4)
+                        rect2 = Rectangle(10, 3)
+
+                        print(rect1.__dict__)
+                        print(rect2.__dict__)
