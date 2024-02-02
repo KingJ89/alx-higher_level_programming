665d47bf32e951f21af12fa654eef220eba3901d
KingJ89 <janmutewera@gmail.com>
2024-02-01 17:08:24 -0800

    Task 2 rectangle completed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,27 +6,40 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        def width(self):
-            return self.__width
+    def width(self):
+        return self.__width
 
-        def width(self, value):
-            if not isinsistance(value, int):
-                raise TypeError("width must be integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    def width(self, value):
 
-            def height(self):
-                return self.__height
+    self._validate_non_negative_integer("width", value)
+    self.__width = value
 
-            def height(self, value):
-                if not isinsistance(value, int):
-                    raise TypeError("height must be an integer")
-                elif value < 0:
-                    raise ValueError("height must be >= 0")
-                self.__height = value
+    def height(self):
+        return self.__height
 
-                def area(self):
-                    return self.__width == 0 or self.__height == 0:
-                        return 0
-                    return 2 * (self.__width + self.__height)
+    def height(self, value):
+        self._validate_non_negative_integer("height", value)
+        self.__height = value
+
+    def _validate_non_negative_integer(self, attribute, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{attribute} must be an integer")
+        if value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
+
+    def area(self):
+        return self.heght * self.width
+
+    def perimeter(self):
+        return 2 * (self.height + self.width) if self.height != 0 and self.width != 0 else 0
+
+    if __name__ == "__main__":
+        my_rectangle = Rectangle(2, 4)
+        print("Area: {} - perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+        print("--")
+
+        my_rectangle.width = 10
+        my_rectangle.height = 3
+        print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
