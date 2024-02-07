ed5e37256c58804a9601e577c7988e482b3f3bb4
KingJ89 <janmutewera@gmail.com>
2024-02-07 12:00:03 -0800

    task 1 my list completed

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this is the 1-my_list module"""
+
+class myList(list):
+    def __init__(self):
+        super().__init__()
+
+        def print_sorted(self):
+            print(sorted(self))
