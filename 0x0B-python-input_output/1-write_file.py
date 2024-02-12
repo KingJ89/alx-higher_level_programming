58145516bf39af88a0063e33677ec92f30c3b772
KingJ89 <janmutewera@gmail.com>
2024-02-11 16:34:29 -0800

    task 1 completed successfully

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""write string from txt file"""
+def write_file(filename="", text=""):
+
+    with open(filename, 'w') as file:
+        return file.write(text)
+
+    if __name__ "__main__":
+        filename = ""
