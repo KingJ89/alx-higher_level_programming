edb142d9d53c41b1a7a5e582b328e4ffa7d96f71
KingJ89 <janmutewera@gmail.com>
2024-05-23 12:21:40 -0700

    Task 16-no_link.sql chores complete

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
+-- script lists all records of second table of database
+-- does not list without rows
+-- displays score and name in that order
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
