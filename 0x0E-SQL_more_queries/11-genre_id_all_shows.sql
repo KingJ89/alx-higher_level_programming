7f875915d4327d241335f0c296a12b3f3df58b7d
KingJ89 <janmutewera@gmail.com>
2024-05-26 00:23:59 -0700

    Task 11 chores handeld

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- lists all tv shows in ascending order
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
