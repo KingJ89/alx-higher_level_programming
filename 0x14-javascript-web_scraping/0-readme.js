d36632351279e8da2d2a25217d0f90efb7edf0bf
KingJ89 <janmutewera@gmail.com>
2024-07-31 10:42:36 -0700

    Task 0-readme.js completed successfully

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Import the filesystem module
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+  console.error('Error reading file:', error);
+  return;
+  }
+  console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+this is a non-empty readme on the ALX project 0x14-javascript-web_scraping by Jan Mutewera
