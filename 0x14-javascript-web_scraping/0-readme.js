8a87cac34360d8b421c1ef7bd5a9dafd7eb3e298
KingJ89 <janmutewera@gmail.com>
2024-07-31 11:33:06 -0700

    Task chores handled successfully

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,8 +7,8 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (error, data) => {
   if (error) {
-  console.error('Error reading file:', error);
+    console.error('Error reading file:', error);
   return;
   }
-  console.log(data);
+    console.log(data);
 });
