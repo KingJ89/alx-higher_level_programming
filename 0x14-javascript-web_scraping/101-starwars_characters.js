57e54d53512568081ab489d1024bf4f7f5e8502f
KingJ89 <janmutewera@gmail.com>
2024-07-31 13:22:24 -0700

    Task 101 starwars caracters completed

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+// Make an HTTP GET request to the API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    const characterPromises = characters.map((characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            reject(charError);
+          } else {
+            const character = JSON.parse(charBody);
+            resolve(character.name);
+          }
+        });
+      });
+    });
+
+    Promise.all(characterPromises)
+      .then((characterNames) => {
+        characterNames.forEach((name) => {
+          console.log(name);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
